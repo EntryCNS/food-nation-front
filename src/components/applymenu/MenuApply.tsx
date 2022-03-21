94b737c04d737acc3f9d0d0800465a6be4c9c568
강성훈 <80798929+ksh5324@users.noreply.github.com>
2022-03-21 18:04:02 +0900

    ♻️:: state, changeFunction

--- src/components/applymenu/MenuApply.tsx
+++ src/components/applymenu/MenuApply.tsx
@@ -1,26 +1,24 @@
 import React, { useCallback, useState } from "react";
-import styled from "styled-components";
-import { lightTheme } from "styles/theme";
 import { MenuContainer } from "components/applymenu/applyListStyle";
 
 const kategorys = ["한식", "일식", "중식", "양식", "디저트", "패스트푸드"];
 
 const MenuApply = () => {
   const [check, setCheck] = useState(8);
-  const [menuName, setMenuName] = useState("");
-  const [menuDes, setMenuDes] = useState("");
-  const menuChange = useCallback(
-    (e) => {
-      setMenuName(e.target.value);
-    },
-    [menuName]
-  );
-  const menuDesChange = useCallback(
-    (e) => {
-      setMenuDes(e.target.value);
-    },
-    [menuDes]
-  );
+  const [inputs, setInputs] = useState({
+    menuName: "",
+    menuDes: "",
+  });
+  const { menuName, menuDes } = inputs;
+
+  const onChange = (e: Event) => {
+    const { value, name }: any = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
   return (
     <MenuContainer>
       <h2 className="menu-container-title">급식 신청</h2>
@@ -43,7 +41,8 @@ const MenuApply = () => {
         type="text"
         value={menuName}
         className="menu-names"
-        onChange={menuChange}
+        onChange={onChange}
+        name="menuName"
         placeholder="메뉴의 이름을 작성해 주세요 (15자 내)"
         maxLength={15}
       />
@@ -52,7 +51,8 @@ const MenuApply = () => {
         maxLength={50}
         placeholder="메뉴의 설명을 작성해 주세요 (50자 내)"
         value={menuDes}
-        onChange={menuDesChange}
+        onChange={onChange}
+        name="menuDes"
       ></textarea>
       <button className="apply-button">신청하기</button>
     </MenuContainer>
