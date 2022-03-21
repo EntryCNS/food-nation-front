2b004feb50a8040c637182a5bb1562aa95cd1f0c
강성훈 <80798929+ksh5324@users.noreply.github.com>
2022-03-21 20:53:31 +0900

    setting

---
+++ src/components/profileBar/ProfileBar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const ProfileBarContainer = styled.div`
+  width: 500px;
+  border: 1px solid black;
+  height: 100vh;
+`;
+
+const ProfileBar = () => {
+  return <ProfileBarContainer></ProfileBarContainer>;
+};
+
+export default ProfileBar;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
+import ProfileBar from "components/profileBar/ProfileBar";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <ProfileBar />
+    </div>
+  );
 };
 
 export default Home;
