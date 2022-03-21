ffceb571d5e24a5601768e146400bd9cd23ec35a
강성훈 <80798929+ksh5324@users.noreply.github.com>
2022-03-21 22:15:05 +0900

    💥:: profile bar

--- src/components/profileBar/ProfileBar.tsx
+++ src/components/profileBar/ProfileBar.tsx
@@ -1,14 +1,32 @@
 import React from "react";
-import styled from "styled-components";
-
-const ProfileBarContainer = styled.div`
-  width: 500px;
-  border: 1px solid black;
-  height: 100vh;
-`;
+import ProfileBarContainer from "components/profileBar/profileBar.ts";
+import Custom1 from "assets/image/profileCustom1.png";
+import Custom3 from "assets/image/profileCustom3.png";
+import Custom4 from "assets/image/profileCustom4.png";
+import Custom2 from "assets/image/profileImage.png";
+import Image from "next/image";
 
 const ProfileBar = () => {
-  return <ProfileBarContainer></ProfileBarContainer>;
+  return (
+    <ProfileBarContainer>
+      <div className="custom1">
+        <Image src={Custom1} />
+      </div>
+      <div className="custom2">
+        <div className="drop">
+          <Image src={Custom2} width={130} height={130} />
+        </div>
+        <h2>강성훈</h2>
+        <p>학생</p>
+      </div>
+      <div className="custom3">
+        <Image src={Custom3} />
+      </div>
+      <div className="custom4">
+        <Image src={Custom4} />
+      </div>
+    </ProfileBarContainer>
+  );
 };
 
 export default ProfileBar;
