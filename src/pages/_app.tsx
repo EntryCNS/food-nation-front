b83118bf9e64ca28d15226696bdfd7699eefc563
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-03-30 22:18:01 +0900

    💥:: setting recoil

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,18 +4,22 @@ import Layout from "components/layout";
 import { ThemeProvider } from "styled-components";
 import { darkTheme, lightTheme } from "styles/theme";
 
+import { RecoilRoot } from "recoil";
+
 import NavBar from "components/navBar";
 
 import ProfileBar from "components/profileBar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <RecoilRoot>
     <ThemeProvider theme={lightTheme}>
       <GlobalStyle />
       <Layout>
         <Component {...pageProps} />
       </Layout>
     </ThemeProvider>
+    </RecoilRoot>
   );
 }
 
