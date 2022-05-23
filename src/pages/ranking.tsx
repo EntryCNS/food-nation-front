bca953eb9f8d0d3880a23026b3bb9740ea3b6f39
배진영 <81845425+baejin1018@users.noreply.github.com>
2022-05-23 17:46:31 +0900

    🔀:: ranking (#28)

---
+++ src/pages/ranking.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Ranking } from "components/ranking/Ranking";
+const ranking = () => {
+  return (
+    <div>
+      <Ranking />
+    </div>
+  );
+};
+
+export default ranking;
