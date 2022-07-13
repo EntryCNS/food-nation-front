6dc6383ebb0a91f01b8f6182b515e72d81944965
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-07-13 09:26:41 +0900

    add menu

--- src/components/reviewComponent/ReviewPage.tsx
+++ src/components/reviewComponent/ReviewPage.tsx
@@ -2,19 +2,15 @@ import React from "react";
 import ReadReview from "./readReview/ReadReview";
 import WriteReview from "./writeReview/WriteReview";
 
+import Menu from "components/common/menu";
+
 import * as R from "./ReviewPage.style";
 
 export default function Review() {
   return (
     <R.ReviewPageWrapper>
       <section>
-        <div
-          style={{
-            width: "580px",
-            height: "330px",
-            backgroundColor: "black",
-          }}
-        ></div>
+        <Menu/>
         <R.ReadReviewWrapper>
         <ReadReview />
         </R.ReadReviewWrapper>
