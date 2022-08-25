67f8ab4af374ca104b6568cb7ee75a80033bfd3a
정우재 <80890472+woojae05@users.noreply.github.com>
2022-08-25 13:37:43 +0900

    Update menu.api.ts

--- src/assets/api/menu.api.ts
+++ src/assets/api/menu.api.ts
@@ -2,10 +2,21 @@ import customAxios from "util/customAxios";
 import IPlanner from "interface/planner/IPlanner";
 class MenuApi {
 
+
+    // 식단표 가져오기
     async getPlannr(year: number, month: number, day: number): Promise<IPlanner[]> {
         const { data } = await customAxios.get(`/menu/planner?year=${year}&month=${month}&day=${day}`)
         return data.planner
     }
+
+
+    // 메뉴 가져오기
+    async getMenu(page: number): Promise<any> {
+        const { data } = await customAxios.get(`/menu?${page}`)
+        return data
+    }
+
+
 }
 
 export default new MenuApi();
