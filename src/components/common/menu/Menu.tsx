0e01e5f007f5544b290364afc858640113a3fde6
정우재 <80890472+woojae05@users.noreply.github.com>
2022-08-25 13:55:40 +0900

    Update Menu.tsx

--- src/components/common/menu/Menu.tsx
+++ src/components/common/menu/Menu.tsx
@@ -16,6 +16,8 @@ const Menu = ({ isCalander }: props) => {
   const { planner, changeDate, year, month, date } = useMenu();
   const today = new Date(year, month, date);
   const day = ["일", "월", "화", "수", "목", "금", "토"];
+  const mealPlanners = ["조식", "중식", "석식"];
+  const imgArray = ["", lunch, dinner];
 
   return (
     <M.Container>
@@ -39,38 +41,27 @@ const Menu = ({ isCalander }: props) => {
           <Arrow id="right" className="svg" />
         </div>
       </M.DateBox>
-      {planner[0] ? (
-        <M.MenuBox>
-          <M.MealBox>
-            <Breakfast />
-            <p>
-              {planner[0] != undefined
-                ? planner[0].menuList.join(", ")
-                : "조식이 없습니다"}
-            </p>
-          </M.MealBox>
 
-          <M.MealBox>
-            <Image src={lunch} width={32} height={32} alt="img" />
-            <p>
-              {planner[1] != undefined
-                ? planner[1].menuList.join(", ")
-                : "중식이 없습니다"}
-            </p>
-          </M.MealBox>
-
-          <M.MealBox>
-            <Image src={dinner} width={32} height={32} alt="img" />
-            <p>
-              {planner[2] != undefined
-                ? planner[2].menuList.join(", ")
-                : "석식이 없습니다"}
-            </p>
-          </M.MealBox>
-        </M.MenuBox>
-      ) : (
-        <h1 id="loading">loading...</h1>
-      )}
+      <M.MenuBox>
+        {mealPlanner.map((item, idx) => {
+          return (
+            <>
+              <M.MealBox>
+                {idx === 0 ? (
+                  <Breakfast />
+                ) : (
+                  <Image src={imgArray[idx]} width={32} height={32} alt="img" />
+                )}
+                <p>
+                  {planner[idx] != undefined
+                    ? planner[idx].menuList.join(", ")
+                    : `${item}이 없습니다`}
+                </p>
+              </M.MealBox>
+            </>
+          );
+        })}
+      </M.MenuBox>
     </M.Container>
   );
 };
